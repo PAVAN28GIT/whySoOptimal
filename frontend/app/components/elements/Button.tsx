e7c750db0936465dac5f99d158d67f2a82e66ae4
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-09-18 12:57:25 +0530

    added both the folders

---
+++ frontend/app/components/elements/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ButtonProps {
+    text: string; // Specify the type of 'text' prop as string
+}
+
+const Button: React.FC<ButtonProps> = ({ text }) => {
+    return (
+        <div className='btn btn-neutral'>
+            {text}
+        </div>
+    );
+};
+
+export default Button;
---
+++ frontend/app/utils/constants.ts
@@ -0,0 +1,19 @@
+type NavbarData = {
+    title: string;
+    path: string;
+}
+
+export const navbarData: NavbarData[] = [
+    {
+        title: 'Home',
+        path: '/',
+    },
+    {
+        title: 'Products',
+        path: '/products',
+    },
+    {
+        title: 'Craftsmen',
+        path: '/craftsmen',
+    },
+]
